7b0a755e53ef073a40dbf2bb674c78b2486aa20f
Sergei <shvets555@mail.ru>
2023-08-07 18:20:38 +0600

    helmet api added

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,9 +1,17 @@
+import { Helmet } from "react-helmet";
 import ComicsList from "../comicsList/ComicsList";
 import AppBanner from "../appBanner/AppBanner";
 
 function ComicsPage() {
   return (
     <>
+        <Helmet>
+          <meta
+              name="description"
+              content="Comics list"
+              />
+          <title>Comics page</title>
+        </Helmet>
         <AppBanner/>
         <ComicsList/>
     </>
